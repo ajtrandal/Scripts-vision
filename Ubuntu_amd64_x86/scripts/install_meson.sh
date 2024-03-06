92945239dd162e233332f80e479d2bba08564d47
ajtrandal <162322008+ajtrandal@users.noreply.github.com>
2024-03-06 11:49:41 +0100

    minor fix

--- Ubuntu_amd64_x86/scripts/install_meson.sh
+++ Ubuntu_amd64_x86/scripts/install_meson.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 if [ $(dpkg-query -W -f='${Status}' meson | grep -c 'ok installed') -eq 0 ]; then
-    wget http://ports.ubuntu.com/pool/universe/m/meson/meson_0.61.2-1_all.deb
-    sudo dpkg -i meson_0.61.2-1_all.deb
-    rm meson_0.61.2-1_all.deb
+#    wget http://ports.ubuntu.com/pool/universe/m/meson/meson_0.61.2-1_all.deb
+#    sudo dpkg -i meson_0.61.2-1_all.deb
+#    rm meson_0.61.2-1_all.deb
+
+     wget http://ports.ubuntu.com/pool/universe/m/meson/meson_1.3.2-1ubuntu1_all.deb
+     sudo dpkg -i meson_1.3.2-1ubuntu1_all.deb
+     rm meson_1.3.2-1ubuntu1_all.deb
+
 else
     echo Meson is already installed
 fi
