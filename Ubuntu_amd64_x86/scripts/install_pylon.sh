99d5e6f0a65f9574fde922866ee2e6ec55611e23
Arne Johan Trandal <arne.johan.trandal@seaonics.com>
2024-03-07 09:56:49 +0100

    added pylon dependencies

--- Ubuntu_amd64_x86/scripts/install_pylon.sh
+++ Ubuntu_amd64_x86/scripts/install_pylon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt install libgl1-mesa-dri libgl1-mesa-glx libxcb-xinerama0 libxcb-xinput0
+
 if [ $(dpkg-query -W -f='${Status}' pylon | grep -c 'ok installed') -eq 0 ]; then
     wget https://www2.baslerweb.com/media/downloads/software/pylon_software/pylon_7_4_0_14900_linux_x86_64_debs.tar.gz
     tar -xzf pylon_7_4_0_14900_linux_x86_64_debs.tar.gz ./pylon_7.4.0.14900-deb0_amd64.deb
