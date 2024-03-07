e2b9fea59f7813dabd38f7fc50e24929bea17d67
ajtrandal <162322008+ajtrandal@users.noreply.github.com>
2024-03-07 20:30:16 +0100

    .

--- Ubuntu_amd64_x86/scripts/install_gstreamer.sh
+++ Ubuntu_amd64_x86/scripts/install_gstreamer.sh
@@ -1,13 +1,26 @@
 # GStreamer
 
-sudo apt install python3-pip
-pip3 install --user meson
+# sudo apt install python3-pip
+sudo apt-get install meson
+sudo apt-get install ninja-build
+
+
+sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio cmake
+export PYLON_ROOT=/opt/pylon
+
+git clone https://github.com/basler/gst-plugin-pylon.git
+cd gst-plugin-pylon
+meson setup builddir --prefix /usr/
+ninja -C builddir
 
 
 
-sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev cmake
-sudo apt install gstreamer1.0-python3-plugin-loader
-sudo apt --fix-broken install
+
+git clone https://github.com/TheImagingSource/tiscamera.git
+
+
+
+
 
 
 # for pylon in default location
