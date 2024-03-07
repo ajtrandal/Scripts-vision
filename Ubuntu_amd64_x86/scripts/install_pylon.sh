f8b52016dd3eba2d94a0676b47eaa0005b9f6edf
ajtrandal <162322008+ajtrandal@users.noreply.github.com>
2024-03-07 20:00:39 +0100

    .

--- Ubuntu_amd64_x86/scripts/install_pylon.sh
+++ Ubuntu_amd64_x86/scripts/install_pylon.sh
@@ -8,7 +8,7 @@ sudo ifconfig enp108s0 mtu 9000
 # ring buffer size to 4096 - Note adaptername only valid for NUC extreme 12700
 sudo ethtool -G enp108s0 rx 4096 tx 4096
 # Configure the interrupt moderation rate  - Note adaptername only valid for NUC extreme 12700
-sudo ethtool -C enp108s0 adaptive-rx off adaptive-tx off rx-usecs 62 tx-usecs 62
+# sudo ethtool -C enp108s0 adaptive-rx off adaptive-tx off rx-usecs 62 tx-usecs 62
 
 if [ $(dpkg-query -W -f='${Status}' pylon | grep -c 'ok installed') -eq 0 ]; then
     wget https://www2.baslerweb.com/media/downloads/software/pylon_software/pylon_7_4_0_14900_linux_x86_64_debs.tar.gz
